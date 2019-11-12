4cca85d704cfe7d7ebe4906043165e74482ff8eb
Kevin Donahue <nonnontrivial@gmail.com>
2019-11-12 14:11:36 -0500

    write json on error

--- index.ts
+++ index.ts
@@ -1,17 +1,22 @@
 import "dotenv/config";
 import { Batcher } from "@botmock-api/client";
 import { default as log } from "@botmock-api/log";
-import { writeJson, remove, mkdirp } from "fs-extra";
+import { writeJson, remove, mkdirp, move } from "fs-extra";
 import { EOL } from "os";
 import { join } from "path";
 import { default as FileWriter } from "./lib/file";
 
+const outputBasename = "output";
+
 /**
- * Moves output data to rasa target
- * @param target path to main rasa project directory
+ * Moves output data to an existing rasa project
+ * @remarks replaces existing files in rasa project
+ * @param pathToRasaProject relative path to rasa project directory
  * @todo
  */
-// async function mv(target: string): Promise<void> {}
+// async function mvOutput(pathToRasaProject: string): Promise<void> {
+//   await move(join(__dirname, outputBasename), pathToRasaProject);
+// }
 
 /**
  * Calls all fetch methods and calls all write methods
@@ -21,18 +26,12 @@ import { default as FileWriter } from "./lib/file";
  * @param args argument vector
  */
 async function main(args: string[]): Promise<void> {
-  const DEFAULT_OUTPUT_DIR = "output";
-  let [, , outputDirectory] = args;
-  if (process.env.OUTPUT_DIR) {
-    outputDirectory = process.env.OUTPUT_DIR
-  }
-  const outputDir = join(__dirname, outputDirectory || DEFAULT_OUTPUT_DIR);
+  const outputDir = join(__dirname, outputBasename);
   log("recreating output directory");
   await remove(outputDir);
   await mkdirp(outputDir);
   log("fetching project data");
-  // @ts-ignore
-  const { data: projectData } = await new Batcher({
+  const { data: projectData }: any = await new Batcher({
     token: process.env.BOTMOCK_TOKEN as string,
     teamId: process.env.BOTMOCK_TEAM_ID as string,
     projectId: process.env.BOTMOCK_PROJECT_ID as string,
@@ -47,6 +46,10 @@ async function main(args: string[]): Promise<void> {
   log("writing files");
   const writer = FileWriter.getInstance({ outputDir, projectData });
   await writer.write();
+  // const [, , pathToRasaProject] = args;
+  // if (pathToRasaProject) {
+  //   await mvOutput(pathToRasaProject);
+  // }
   log("done");
 }
 
@@ -55,10 +58,6 @@ process.on("uncaughtException", () => {});
 
 main(process.argv).catch(async (err: Error) => {
   log(err.stack as string, { isError: true });
-  if (process.env.OPT_IN_ERROR_REPORTING) {
-    // Sentry.captureException(err);
-  } else {
-    const { message, stack } = err;
-    await writeJson(join(__dirname, "err.json"), { message, stack }, { EOL, spaces: 2 });
-  }
+  const { message, stack } = err;
+  await writeJson(join(__dirname, "err.json"), { message, stack }, { EOL, spaces: 2 });
 });
